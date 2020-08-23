c23b82abd226107aec613ca260e217ad5e238185
= <nidhalbacc@gmail.com>
2020-08-23 18:23:31 +0200

    deployed to heroku

---
+++ heroku-deploy/build/precache-manifest.8230f8736b8006e6c9fc0dd8f216c609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1471e97a112731727da431fd583da3d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621cb295e063468d8fb2",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4e1f8a3260ad801e2279",
+    "url": "/static/js/2.b80d1c2f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.b80d1c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621cb295e063468d8fb2",
+    "url": "/static/js/main.20e04e99.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
